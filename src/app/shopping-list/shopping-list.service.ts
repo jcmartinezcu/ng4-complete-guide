952aa204863c3911f10324d9a8e8e7e30d02412c
jcmartinez <jcmartinezcu@gmail.com>
2020-07-22 13:27:06 +0200

    video 120

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,13 @@
 import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
 
 export class ShoppingListService{
-   private ingredients: Ingredient[] = [
-        new Ingredient('Apple', 5),
-        new Ingredient('Tomatoes', 10)
+       ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+       private ingredients: Ingredient[] = [
+            new Ingredient('Apple', 5),
+            new Ingredient('Tomatoes', 10)
       ];
 
       getIngredients(){
@@ -12,5 +16,6 @@ export class ShoppingListService{
 
       addIngredient(ingredient: Ingredient){
           this.ingredients.push(ingredient);
+          this.ingredientsChanged.emit(this.ingredients.slice());
       }
 }
