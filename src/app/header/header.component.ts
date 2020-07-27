cb8fcd43973b8e27f27e90cbbff6299427c6c74a
jcmartinez <jcmartinezcu@gmail.com>
2020-07-27 12:37:00 +0200

    video 156

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component} from '@angular/core';
 
 
 @Component({
@@ -7,8 +7,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent{
 
-    @Output() featuresSelected = new EventEmitter<string>();
-    onSelect(features: string){
-        this.featuresSelected.emit(features);
-    }
 }
