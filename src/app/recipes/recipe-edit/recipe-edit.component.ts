6375cef13ddb0c48a893114217543853da3163ef
jcmartinez <jcmartinezcu@gmail.com>
2020-07-31 10:28:18 +0200

    video 233

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
@@ -16,7 +16,8 @@ export class RecipeEditComponent implements OnInit {
   recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute, 
-              private recicipeService: RecipeService) { }
+              private recicipeService: RecipeService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.route.params
@@ -41,6 +42,7 @@ export class RecipeEditComponent implements OnInit {
     else{
       this.recicipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
 
   onIngredient(){
@@ -84,6 +86,10 @@ export class RecipeEditComponent implements OnInit {
 
   getControls(){
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  onCancel(){
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
 }
