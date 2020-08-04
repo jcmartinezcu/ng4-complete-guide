575b77f836af96c22a3bf9b51573deeb2becf021
jcmartinez <jcmartinezcu@gmail.com>
2020-08-04 08:54:50 +0200

    video 280

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 
 @Component({
@@ -7,4 +8,9 @@ import { Component} from '@angular/core';
 })
 export class HeaderComponent{
 
+    constructor(private dataStoreService: DataStorageService){}
+
+    onSaveData(){
+        this.dataStoreService.storeRecipe();
+    }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
-@Injectable({providedIn: 'root'})
-export class DataStorageService{
-    constructor(private http: HttpClientModule){}
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  urlFireBase = 'https://ng-course-recipe-book-8a357.firebaseio.com/';
 
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipe() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(this.urlFireBase + 'recipes.json', recipes)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
