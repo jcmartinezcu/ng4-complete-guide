96e638f044a665e7f33d6c7212d93491b0185ee8
jcmartinez <jcmartinezcu@gmail.com>
2020-08-04 10:17:49 +0200

    video 283

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -15,6 +15,6 @@ export class HeaderComponent{
     }
 
     onFetchData(){
-        this.dataStoreService.fetchRecipes();
+        this.dataStoreService.fetchRecipes().subscribe();
     }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map } from 'rxjs/operators'
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -19,14 +19,20 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(this.urlFireBase)
-        .pipe(map(recipes =>{
-            return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
-            })
-        }))
-        .subscribe((recipes) => {
+    return this.http
+      .get<Recipe[]>(this.urlFireBase)
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap(recipes =>{
             this.recipeService.setRecipes(recipes);
-    });
+        })
+      )
   }
 }
