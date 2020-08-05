a84fc80b5863537c89a49fa963b3d8edc671f1e5
jcmartinez <jcmartinezcu@gmail.com>
2020-08-05 14:36:36 +0200

    video 302

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -30,6 +30,10 @@ export class HeaderComponent implements OnInit, OnDestroy{
     onFetchData(){
         this.dataStoreService.fetchRecipes().subscribe();
     }
+
+    onLogout(){
+        this.authService.logout();
+    }
     ngOnDestroy(){
         this.userSub.unsubscribe();
     }
