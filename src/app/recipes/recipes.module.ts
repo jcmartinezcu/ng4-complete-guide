79285a80c4713e30535ae4ed8352f018ec2a0f56
jcmartinez <jcmartinezcu@gmail.com>
2020-08-06 11:22:18 +0200

    video 322

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,6 +5,9 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,7 @@ import { RecipeStartComponent } from './recipe-start/recipe-start.component';
     RecipeStartComponent,
     RecipeEditComponent
   ],
+  imports:[RouterModule, CommonModule, ReactiveFormsModule],
   exports:[
     RecipesComponent,
     RecipeListComponent,
