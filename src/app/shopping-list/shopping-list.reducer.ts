8e60fda8a82a4dc00f044f4b30f44051ae09e6b7
jcmartinez <jcmartinezcu@gmail.com>
2020-08-07 10:19:52 +0200

    video 347

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,13 +1,20 @@
 import { Ingredient } from '../shared/ingredient.model';
+import { Action } from '@ngrx/store';
 
 const initiaState = {
-    Ingredient: [
+    ingredient: [
         new Ingredient('Apple', 5),
         new Ingredient('Tomatoes', 10)
   ]
 }
 
 
-export function ShoppingListReducer(state= initiaState, action){
-
+export function ShoppingListReducer(state= initiaState, action: Action){
+    switch(action.type){
+        case 'ADD_INGREDIENT' :
+            return {
+                ...state,
+                ingredient: [...state.ingredient, action]
+            }
+    }
 }
