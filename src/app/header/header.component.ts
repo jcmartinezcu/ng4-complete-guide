35708d6184a82887973779ef58a9d6942fb52adc
jcmartinez <jcmartinezcu@gmail.com>
2020-08-08 20:37:19 +0200

    video 362

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, OnDestroy} from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import * as fromApp from '../store/app.reduce';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -13,10 +16,12 @@ export class HeaderComponent implements OnInit, OnDestroy{
     private userSub: Subscription;
     isAuthenticated = false;
 
-    constructor(private dataStoreService: DataStorageService, private authService: AuthService){}
+    constructor(private dataStoreService: DataStorageService, private authService: AuthService, private store: Store<fromApp.AppState>){}
 
     ngOnInit(){
-       this.userSub = this.authService.user.subscribe(user=>{
+       this.userSub = this.store.select('auth').pipe(map(authState =>{
+           return authState.user;
+       })).subscribe(user=>{
            this.isAuthenticated = !!user;
            console.log(!user);
            console.log(!!user);
